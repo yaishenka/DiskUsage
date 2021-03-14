038fdd4f20ac91ecd81e4721eeba8db6d71cb231
yaishenka <gagarinovdaniil@gmail.com>
2021-03-14 20:46:10 +0300

    add script

---
+++ disk_usage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dir="."
+
+if [[ $# -ge 1 ]]
+then
+	dir=$1
+fi
+
+du -sh $dir
+
+
+ls -AkhlR $dir | awk -v file_name="$dir" '{
+	if ($2 ~ /K$/) total += $2;
+	if ($2 ~ /M$/) total += $2 * 1024;
+	if ($2 ~ /G$/) total += $2 * 1024 * 1024;
+} END {
+	total += 4;
+	if (total < 1024) printf "%dK\t", total;
+	else if (total < 1024 * 1024) printf "%.1fM\t", total / 1024;
+	else printf "%.1fG\t", total / 1024 / 1024;
+	printf "%s\n", file_name;	
+}'
